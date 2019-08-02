2d1cc1cccc4356b751de8f660fc699960edb326e
Rishit Khare <AdarshKhare@Home-1s-MacBook-Pro.local>
2019-08-01 23:33:11 -0700

    August 1
    Version of Tetris as of August 1st

--- Tetris.py
+++ Tetris.py
@@ -50,6 +50,8 @@ class Block:
             self.y += 1
         else:
             self.moving = False
+    def willMoveSide(self):
+        pass
     def moveSide(self, direction):
         willMove = True
         for square in self.grid.blocks:
@@ -74,8 +76,53 @@ class Block:
 
 
 
+class TetraBlock:
+    """A group of four blocks that will fall onto the screen."""
+    def __init__(self, window):
+        self.window = window
+        self.r = randint(0,255)
+        self.g = randint(0,255)
+        self.b = randint(0,255)
+        self.x = randint(1,self.window.width-1)
+
+        block_types = [ [ (0,0), (0,1),  (0,-1),  (1,0)  ],  # T
+                        [ (0,0), (0,1),  (0,2),   (0,-1) ],  # I
+                        [ (0,0), (0,1),  (1,1),  (-1,0)  ],  # S
+                        [ (0,0), (0,-1), (1,-1), (-1,0)  ],  # Z
+                        [ (0,0), (0,1),  (1,1),   (1,0)  ],  # O
+                        [ (0,0), (-1,1), (-1,0),  (1,0)  ],  # L
+                        [ (0,0), (1,1),  (-1,0),  (1,0)  ],  # J
+                        ]
+        
+        self.type = randint(0,6)
+        self.pieces = []
+        for x in range(0,4):
+            X_pos, Y_pos = block_types[self.type][x]
+            self.pieces.append(Block(self.grid, self.r, self.g, self.b, X_pos+self.x,Y_pos+2))
+    def moveDown(self):
+
+        movedown = True
+        for piece in self.pieces:
+            for block in BG:
+                if block.x == piece.x-1:
+                    movedown = False
+        if movedown:
+            for piece in self.pieces:
+                pieces.moveDown
+                
+
+    def moveSide(self):
+        moveside = True
+        for piece in self.pieces:
+            for block in BG.blocks:
+                pass
+    def draw(self):
+        for piece in self.pieces:
+            piece.draw()
 
 
+
+       
 window = pygame.display.set_mode([750,650])
 c = pygame.time.Clock()
 
@@ -100,7 +147,7 @@ pygame.mixer.music.play(-1)
 
 running = True
 
-##### ----- MAIN LOOP ----- #####
+############# ------------- MAIN LOOP ------------- #############
 
 while running:
     for event in pygame.event.get():
@@ -118,6 +165,7 @@ while running:
     if keys[pygame.K_DOWN]:
         if BG.grav_timer > 100:
             BG.grav_timer = 0
+            
             moving_block.moveDown()
     else:
         if BG.grav_timer > 500:
@@ -146,6 +194,11 @@ while running:
         if counter == BG.width:
             for square in removed:
                 BG.blocks.remove(square)
+            for x in range(1,BG.height+1):
+                for square in BG.blocks:
+                    if square.y == layer - x:
+                        square.moveDown()
+                
     pygame.display.flip()
     c.tick(30)
 
